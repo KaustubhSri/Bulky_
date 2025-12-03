0f7bf160c5854940f62153d4b48b67bd215aa5cd
kaustubh.srivastava <kaustubh.srivastava@E1DV596>
2025-12-03 18:43:54 +0530

    changes Done

--- bulkylatest/Controllers/HomeController.cs
+++ bulkylatest/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace BulkyWeb.Controllers
             _logger = logger;
         } 
 
-        public IActionResult Index()  
+        public IActionResult Index1()  
         {
             return View();
         }
