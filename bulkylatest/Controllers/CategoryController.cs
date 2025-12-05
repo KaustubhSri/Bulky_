25500b98b4f674785af05178337b5ce067882f2f
kaustubh.srivastava <kaustubh.srivastava@E1DV596>
2025-12-05 10:37:38 +0530

    5/12/2025 commit

--- bulkylatest/Controllers/CategoryController.cs
+++ bulkylatest/Controllers/CategoryController.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace bulkylatest.Controllers
 {
     public class CategoryController : Controller
     {
-        
+        public readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {  _db = db; }
         public IActionResult Index()
         {
             return View();
